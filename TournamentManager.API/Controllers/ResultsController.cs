45c3e65e046614fd1b3c890625708141225fd71d
Paul Pitchford <paul.pitchford@yourhomecare.co.uk>
2023-02-13 16:35:25 +0000

    Added results controller

---
+++ TournamentManager.API/Controllers/ResultsController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TournamentManager.Infrastructure.Entities;
+using TournamentManager.Infrastructure.Interfaces;
+
+namespace TournamentManager.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ResultsController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ResultsController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpGet]
+        [Route("GetResultsByGame/{gameId}")]
+        public IEnumerable<Result> GetAllResults(Guid gameId)
+        {
+            return _unitOfWork.Results.GetResultsByGame(gameId);
+        }
+
+        [HttpGet]
+        [Route("GetResultsByPlayer/{playerId}")]
+        public IEnumerable<Result> GetResultsBySeason(Guid playerId)
+        {
+            return _unitOfWork.Results.GetResultsByPlayer(playerId);
+        }
+
+        [HttpPost]
+        public int AddResult([FromBody] Result result)
+        {
+            _unitOfWork.Results.Add(result);
+            return _unitOfWork.Save();
+        }
+
+        [HttpGet("{Id}")]
+        public Result GetResult(Guid Id)
+        {
+            return _unitOfWork.Results.GetById(Id);
+        }
+
+        [HttpPut("{Id}")]
+        public bool UpdateResult(Guid Id, [FromBody] Result result)
+        {
+            Result? oldResult = _unitOfWork.Results.GetById(Id);
+            if (oldResult != null)
+            {
+                oldResult.GameId = result.GameId;
+                oldResult.PlayerId = result.PlayerId;
+                oldResult.Position = result.Position;
+                oldResult.Cash = result.Cash;
+                oldResult.Points = result.Points;
+                _unitOfWork.Save();
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
