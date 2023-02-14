be31f9bf95423da4c5ed3f71c6a405bd67ac4a76
Paul Pitchford <paul.pitchford@yourhomecare.co.uk>
2023-02-14 12:15:42 +0000

    Validator added for gametype forms

---
+++ TournamentManager.Web/Validators/GameTypeValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using TournamentManager.Infrastructure.Entities;
+
+namespace TournamentManager.Web.Validators
+{
+    public class GameTypeValidator : AbstractValidator<GameType>
+    {
+        public GameTypeValidator()
+        {
+            RuleFor(gt => gt.GameTypeName).NotEmpty().WithMessage("Please enter a game type name");
+        }
+    }
+}
