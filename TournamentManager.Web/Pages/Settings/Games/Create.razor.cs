ce6de9249cbd69cba510333b3741057d235e1b2a
Paul Pitchford <paul.pitchford@yourhomecare.co.uk>
2023-02-15 18:35:57 +0000

    Added Game CRUD pages

---
+++ TournamentManager.Web/Pages/Settings/Games/Create.razor.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Components;
+using TournamentManager.Infrastructure.Entities;
+using TournamentManager.Web.Components;
+using TournamentManager.Web.Services;
+
+namespace TournamentManager.Web.Pages.Settings.Games
+{
+    public partial class Create
+    {
+        [Inject]
+        WebApiService _apiClient { get; set; } = default!;
+
+        [Inject]
+        NavigationManager _navManager { get; set; } = default!;
+
+        Game Game { get; set; } = new Game { GameDateTime = DateTime.Now };
+        IEnumerable<Season>? Seasons { get; set; }
+        IEnumerable<GameType>? GameTypes { get; set; }
+        IEnumerable<Venue>? Venues { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            Seasons = await _apiClient.httpClient.GetFromJsonAsync<IEnumerable<Season>>("/api/Seasons/");
+            GameTypes = await _apiClient.httpClient.GetFromJsonAsync<IEnumerable<GameType>>("/api/GameTypes/");
+            Venues = await _apiClient.httpClient.GetFromJsonAsync<IEnumerable<Venue>>("/api/Venues/");
+        }
+
+        async Task OnSubmit()
+        {
+            try
+            {
+                var response = await _apiClient.httpClient.PostAsJsonAsync<Game>("/api/Games/", Game);
+                int gameId = await response.Content.ReadFromJsonAsync<int>();
+                if (gameId > 0)
+                {
+                    _navManager.NavigateTo("/settings/games");
+                }
+            }
+            catch (Exception ex)
+            {
+                AlertIsVisible = true;
+                Message = ex.Message;
+                MessageType = AlertMessageType.Danger;
+            }
+        }
+
+        void OnCancel()
+        {
+            _navManager.NavigateTo("/settings/games");
+        }
+    }
+}
---
+++ TournamentManager.Web/Pages/Settings/Games/Edit.razor.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Components;
+using TournamentManager.Infrastructure.Entities;
+using TournamentManager.Web.Components;
+using TournamentManager.Web.Services;
+
+namespace TournamentManager.Web.Pages.Settings.Games
+{
+    public partial class Edit
+    {
+        [Inject]
+        WebApiService _apiClient { get; set; } = default!;
+
+        [Inject]
+        NavigationManager _navManager { get; set; } = default!;
+
+        [Parameter]
+        public Guid Id { get; set; }
+        Game? Game { get; set; }
+        IEnumerable<Season>? Seasons { get; set; }
+        IEnumerable<GameType>? GameTypes { get; set; }
+        IEnumerable<Venue>? Venues { get; set; }
+
+        bool formLoading;
+
+        protected override async Task OnInitializedAsync()
+        {
+            formLoading = true;
+            Game = await _apiClient.httpClient.GetFromJsonAsync<Game>($"/api/Games/{Id}");
+
+            Seasons = await _apiClient.httpClient.GetFromJsonAsync<IEnumerable<Season>>("/api/Seasons/");
+            GameTypes = await _apiClient.httpClient.GetFromJsonAsync<IEnumerable<GameType>>("/api/GameTypes/");
+            Venues = await _apiClient.httpClient.GetFromJsonAsync<IEnumerable<Venue>>("/api/Venues/");
+            formLoading = false;
+        }
+
+        async Task OnSubmit()
+        {
+            if (Game != null)
+            {
+                try
+                {
+                    var response = await _apiClient.httpClient.PutAsJsonAsync<Game>($"/api/Games/{Id}", Game);
+                    bool updated = await response.Content.ReadFromJsonAsync<bool>();
+
+                    if (updated)
+                    {
+                        _navManager.NavigateTo("/settings/games");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    AlertIsVisible = true;
+                    Message = ex.Message;
+                    MessageType = AlertMessageType.Danger;
+                }
+            }
+        }
+
+        void OnCancel()
+        {
+            _navManager.NavigateTo("/settings/games");
+        }
+    }
+}
