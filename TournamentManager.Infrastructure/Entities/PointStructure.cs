9795158a3d2a04806992c73358e261962df3ebb7
Paul Pitchford <paul.pitchford@yourhomecare.co.uk>
2023-02-16 20:37:07 +0000

    Note update

--- TournamentManager.Infrastructure/Entities/PointStructure.cs
+++ TournamentManager.Infrastructure/Entities/PointStructure.cs
@@ -9,8 +9,8 @@ namespace TournamentManager.Infrastructure.Entities
     public class PointStructure
     {
         public Guid Id { get; set; }
-        // If no points are added to a position, this is the default amount that will be used
         public string? PointStructureDescription { get; set; }
+        // If no points are added to a position, this is the default amount that will be used
         public double DefaultPoints { get; set; }
 
         public virtual ICollection<PointPosition> PointPositions { get; set; } = new HashSet<PointPosition>();
