b99e3a1eb21d1d0e21a6db9f6c69ec7be2d01071
Paul Pitchford <paul.pitchford@yourhomecare.co.uk>
2023-02-17 13:43:55 +0000

    Fixed a bug where the next number for position wouldn't calculate proper if the list was unsorted or it we were opening and exisitng structure.

--- TournamentManager.Infrastructure/BusinessLogic/Points.cs
+++ TournamentManager.Infrastructure/BusinessLogic/Points.cs
@@ -9,6 +9,10 @@ namespace TournamentManager.Infrastructure.BusinessLogic
             // This method takes the list and returns the next lowest value down to 1.
             // If 2 is the first number in the collection, 1 is returned.
             // If no missing number is found it basically returns max + 1.
+
+            // First we sort it
+            numbers = numbers.Order().ToList();
+
             int expected = 1;
             foreach (int number in numbers)
             {
