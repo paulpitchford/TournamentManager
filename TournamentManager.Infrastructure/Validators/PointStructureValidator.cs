545aaecaaf2b34455ede4b5b5961abe9b6f58012
Paul Pitchford <paul.pitchford@yourhomecare.co.uk>
2023-02-18 19:08:22 +0000

    Added some extra validation so that when a points structure is saved it checks the points positions run 1..10 without any missing gaps. E.g. 1..3 - 5..10. In this instance 4 is missing. The validation will fail in this instance.

--- TournamentManager.Infrastructure/Validators/PointStructureValidator.cs
+++ TournamentManager.Infrastructure/Validators/PointStructureValidator.cs
@@ -1,19 +1,19 @@
 ﻿using FluentValidation;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using TournamentManager.Infrastructure.Entities;
+using TournamentManager.Infrastructure.Interfaces;
 
 namespace TournamentManager.Infrastructure.Validators
 {
     public class PointStructureValidator : AbstractValidator<PointStructure>
     {
-        public PointStructureValidator()
+        private readonly IPoints _points;
+        public PointStructureValidator(IPoints points)
         {
+            _points = points;
+
             RuleFor(ps => ps.PointStructureDescription).NotEmpty().WithMessage("Please enter a point structure description").MaximumLength(50).WithMessage("The maximum character length is 50");
             RuleFor(ps => ps.DefaultPoints).NotEmpty().WithMessage("Please select the default number of points for all places");
+            RuleFor(ps => ps.PointPositions).Must(pp => pp.Max(p => p.Position) + 1 == _points.NextPosition(pp.Select(p => p.Position).ToList())).WithMessage("The list of positions does not run equal. Pleas ensure they run equal e.g. 1-10.");
         }
     }
 }
