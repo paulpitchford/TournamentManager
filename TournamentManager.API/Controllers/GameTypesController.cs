624cafa511eb800fb250ba1c1006e1cc58578334
Paul Pitchford <paul.pitchford@yourhomecare.co.uk>
2023-02-18 15:01:02 +0000

    Updated game types controller.

--- TournamentManager.API/Controllers/GameTypesController.cs
+++ TournamentManager.API/Controllers/GameTypesController.cs
@@ -16,39 +16,76 @@ namespace TournamentManager.API.Controllers
         }
 
         [HttpGet]
-        public IEnumerable<GameType> GetAllGameTypes()
+        public ActionResult<IEnumerable<GameType>> GetAllGameTypes()
         {
-            return _unitOfWork.GameTypes.GetAllAscending();
+            try
+            {
+                IEnumerable<GameType> GameTypes = _unitOfWork.GameTypes.GetAllAscending();
+                return Ok(GameTypes);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"There was an error retrieving data from the server: {ex.Message}");
+            }
         }
 
         [HttpPost]
-        public int AddGameType([FromBody] GameType gameType)
+        public ActionResult<int> AddGameType([FromBody] GameType gameType)
         {
-            _unitOfWork.GameTypes.Add(gameType);
-            return _unitOfWork.Save();
+            try
+            {
+                _unitOfWork.GameTypes.Add(gameType);
+                return Ok(_unitOfWork.Save());
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"There was an error saving your data: {ex.Message}");
+            }
         }
 
         [HttpGet("{Id}")]
-        public GameType GetGameType(Guid Id)
+        public ActionResult<GameType> GetGameType(Guid Id)
         {
-            return _unitOfWork.GameTypes.GetById(Id);
+            try
+            {
+                GameType gameType = _unitOfWork.GameTypes.GetById(Id);
+                if (gameType != null)
+                {
+                    return gameType;
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"There was an error retrieving data from the server: {ex.Message}");
+            }
         }
 
         [HttpPut("{Id}")]
-        public bool UpdateGameType(Guid Id, [FromBody] GameType gameType)
+        public ActionResult<bool> UpdateGameType(Guid Id, [FromBody] GameType gameType)
         {
-            GameType? oldGameType = _unitOfWork.GameTypes.GetById(Id);
-            if (oldGameType != null)
+            try
             {
-                oldGameType.GameTypeName = gameType.GameTypeName;
-                oldGameType.AwardPoints = gameType.AwardPoints;
-                oldGameType.IsDefault = gameType.IsDefault;
-                _unitOfWork.Save();
-                return true;
+                GameType? oldGameType = _unitOfWork.GameTypes.GetById(Id);
+                if (oldGameType != null)
+                {
+                    oldGameType.GameTypeName = gameType.GameTypeName;
+                    oldGameType.AwardPoints = gameType.AwardPoints;
+                    oldGameType.IsDefault = gameType.IsDefault;
+                    _unitOfWork.Save();
+                    return true;
+                }
+                else
+                {
+                    return NotFound();
+                }
             }
-            else
+            catch (Exception ex)
             {
-                return false;
+                return BadRequest($"There was an error saving your data: {ex.Message}");
             }
         }
     }
