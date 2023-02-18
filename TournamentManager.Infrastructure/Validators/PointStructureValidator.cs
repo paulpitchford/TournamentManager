2e88a0ae20d131e6b6405ea436e8c0a6abb6677d
Paul Pitchford <paul.pitchford@yourhomecare.co.uk>
2023-02-18 19:28:43 +0000

    Added test to point structure validator.

--- TournamentManager.Infrastructure/Validators/PointStructureValidator.cs
+++ TournamentManager.Infrastructure/Validators/PointStructureValidator.cs
@@ -15,8 +15,8 @@ namespace TournamentManager.Infrastructure.Validators
             RuleFor(ps => ps.PointStructureDescription).NotEmpty().WithMessage("Please enter a point structure description").MaximumLength(50).WithMessage("The maximum character length is 50");
             RuleFor(ps => ps.DefaultPoints).NotEmpty().WithMessage("Please select the default number of points for all places");
 
-            //This checks the points positions run 1..10 without any missing gaps. E.g. 1..3 - 5..10.In this instance 4 is missing.The validation will fail in this instance.
-            RuleFor(ps => ps.PointPositions).Must(pp => pp.Max(p => p.Position) + 1 == _points.NextPosition(pp.Select(p => p.Position).ToList())).WithMessage("The list of positions does not run equal. Pleas ensure they run equal e.g. 1-10.");
+            //This checks the points positions run 1..10 without any missing gaps. E.g. 1..3 - 5..10.In this instance 4 is missing.The validation will fail in this instance. Only valid when the collection has 1 or more items.
+            RuleFor(ps => ps.PointPositions).Must(pp => pp.Max(p => p.Position) + 1 == _points.NextPosition(pp.Select(p => p.Position).ToList())).When(pp => pp.PointPositions.Count > 0).WithMessage("The list of positions does not run equal. Pleas ensure they run equal e.g. 1-10.");
         }
     }
 }
--- TournamentManager.Tests/Validators/PointStructureValidatorTests.cs
+++ TournamentManager.Tests/Validators/PointStructureValidatorTests.cs
@@ -1,21 +1,22 @@
 ﻿using FluentValidation.TestHelper;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using TournamentManager.Infrastructure.Entities;
+using TournamentManager.Infrastructure.Enums;
+using TournamentManager.Infrastructure.Interfaces;
 using TournamentManager.Infrastructure.Validators;
+using TournamentManager.Tests.Fixtures;
 
 namespace TournamentManager.Tests.Validators
 {
-    public class PointStructureValidatorTests
+    public class PointStructureValidatorTests : IClassFixture<PointsFixture>
     {
+        private PointsFixture _fixture;
+
         private PointStructureValidator validator;
 
-        public PointStructureValidatorTests()
+        public PointStructureValidatorTests(PointsFixture fixture)
         {
-            validator = new PointStructureValidator();
+            _fixture = fixture;
+            validator = new PointStructureValidator(_fixture.Points);
         }
 
         [Fact]
@@ -68,6 +69,86 @@ namespace TournamentManager.Tests.Validators
 
             // Assert
             result.ShouldNotHaveValidationErrorFor(p => p.DefaultPoints);
+        }
+
+        [Fact]
+        public void ShouldNotFailIfListOfPositionsRunEqual()
+        {
+            // A structures children should run equal, e.g. 1..10, not 1..3 - 5..10 (4 missing). This test ensures the rule fails.
+
+            // Arrange
+            PointStructure pointStructure = new PointStructure
+            {
+                Id = Guid.NewGuid(),
+                PointStructureDescription = "Test",
+                DefaultPoints = 15,
+                PointPositions = new List<PointPosition>
+                {
+                    new PointPosition
+                    {
+                        Position = 1, Points = 200, MultiplierType = PointPositionMultiplierType.PlayerCount, MultiplierValue = 0
+                    },
+                    new PointPosition
+                    {
+                        Position = 2, Points = 150, MultiplierType = PointPositionMultiplierType.PlayerCount, MultiplierValue = 0
+                    },
+                    // 3 is missing in this example
+                    new PointPosition
+                    {
+                        Position = 3, Points = 100, MultiplierType = PointPositionMultiplierType.PlayerCount, MultiplierValue = 0
+                    },
+                    new PointPosition
+                    {
+                        Position = 4, Points = 50, MultiplierType = PointPositionMultiplierType.PlayerCount, MultiplierValue = 0
+                    },
+                }
+            };
+
+            // Act
+            var result = validator.TestValidate(pointStructure);
+
+            // Assert
+            result.ShouldNotHaveValidationErrorFor(p => p.PointPositions);
+        }
+
+        [Fact]
+        public void ShouldFailIfListOfPositionsDoesNotRunEqual()
+        {
+            // A structures children should run equal, e.g. 1..10, not 1..3 - 5..10 (4 missing). This test ensures the rule fails.
+
+            // Arrange
+            PointStructure pointStructure = new PointStructure
+            {
+                Id = Guid.NewGuid(),
+                PointStructureDescription = "Test",
+                DefaultPoints = 15,
+                PointPositions = new List<PointPosition>
+                {
+                    new PointPosition 
+                    {
+                        Position = 1, Points = 200, MultiplierType = PointPositionMultiplierType.PlayerCount, MultiplierValue = 0
+                    },
+                    new PointPosition
+                    {
+                        Position = 2, Points = 150, MultiplierType = PointPositionMultiplierType.PlayerCount, MultiplierValue = 0
+                    },
+                    // 3 is missing in this example
+                    new PointPosition
+                    {
+                        Position = 4, Points = 100, MultiplierType = PointPositionMultiplierType.PlayerCount, MultiplierValue = 0
+                    },
+                    new PointPosition
+                    {
+                        Position = 5, Points = 50, MultiplierType = PointPositionMultiplierType.PlayerCount, MultiplierValue = 0
+                    },
+                }
+            };
+
+            // Act
+            var result = validator.TestValidate(pointStructure);
+
+            // Assert
+            result.ShouldHaveValidationErrorFor(p => p.PointPositions);
         }
     }
 }
