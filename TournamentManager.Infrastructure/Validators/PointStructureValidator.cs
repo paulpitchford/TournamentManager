81efbd31308effcee0be97d55a827d4beea87ff8
Paul Pitchford <paul.pitchford@yourhomecare.co.uk>
2023-02-18 19:08:43 +0000

    Missed commiting file

--- TournamentManager.Infrastructure/Validators/PointStructureValidator.cs
+++ TournamentManager.Infrastructure/Validators/PointStructureValidator.cs
@@ -1,4 +1,5 @@
 ﻿using FluentValidation;
+using System.Reflection;
 using TournamentManager.Infrastructure.Entities;
 using TournamentManager.Infrastructure.Interfaces;
 
@@ -13,6 +14,8 @@ namespace TournamentManager.Infrastructure.Validators
 
             RuleFor(ps => ps.PointStructureDescription).NotEmpty().WithMessage("Please enter a point structure description").MaximumLength(50).WithMessage("The maximum character length is 50");
             RuleFor(ps => ps.DefaultPoints).NotEmpty().WithMessage("Please select the default number of points for all places");
+
+            //This checks the points positions run 1..10 without any missing gaps. E.g. 1..3 - 5..10.In this instance 4 is missing.The validation will fail in this instance.
             RuleFor(ps => ps.PointPositions).Must(pp => pp.Max(p => p.Position) + 1 == _points.NextPosition(pp.Select(p => p.Position).ToList())).WithMessage("The list of positions does not run equal. Pleas ensure they run equal e.g. 1-10.");
         }
     }
