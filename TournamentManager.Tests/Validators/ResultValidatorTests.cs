81899e7a0d3dcc7da790c6de8bf1284ade403bee
Paul Pitchford <paul.pitchford@yourhomecare.co.uk>
2023-02-20 16:40:11 +0000

    Added results validator and removed results input from Game create form. We're never going to be adding results when we add the game.

---
+++ TournamentManager.Tests/Validators/ResultValidatorTests.cs
@@ -0,0 +1,48 @@
+﻿using FluentValidation.TestHelper;
+using PokerTournament.Infrastructure.Validators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TournamentManager.Infrastructure.Entities;
+using TournamentManager.Infrastructure.Validators;
+
+namespace TournamentManager.Tests.Validators
+{
+    public class ResultValidatorTests
+    {
+        private ResultValidator validator;
+
+        public ResultValidatorTests()
+        {
+            validator = new ResultValidator();
+        }
+
+        [Fact]
+        public void ShouldHaveErrorWhenPlayerIsNull()
+        {
+            // Arrange
+            Result result = new Result { };
+
+            // Act
+            var validationResult = validator.TestValidate(result);
+
+            // Assert
+            validationResult.ShouldHaveValidationErrorFor(r => r.PlayerId);
+        }
+
+        [Fact]
+        public void ShouldNotHaveErrorWhenPlayerIsSpecified()
+        {
+            // Arrange
+            Result result = new Result { PlayerId = new Guid("644d7d1a-57d1-4e70-9963-376369fa73cd") };
+
+            // Act
+            var validationResult = validator.TestValidate(result);
+
+            // Assert
+            validationResult.ShouldNotHaveValidationErrorFor(p => p.PlayerId);
+        }
+    }
+}
