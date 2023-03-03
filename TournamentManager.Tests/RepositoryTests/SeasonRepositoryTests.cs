b0a7c473f79f3da6df4a5dd95c3c31910cbcbc88
Paul Pitchford <paul.pitchford@yourhomecare.co.uk>
2023-03-03 10:01:11 +0000

    Added a test to check removing of entities to season. 1. it fails. 2. 5 other tests seem to failing. Coming back to this later.

--- TournamentManager.Tests/RepositoryTests/SeasonRepositoryTests.cs
+++ TournamentManager.Tests/RepositoryTests/SeasonRepositoryTests.cs
@@ -103,5 +103,66 @@ namespace TournamentManager.Tests.RepositoryTests
                 }
             }
         }
+
+        [Fact]
+        public void CanRemoveSeason()
+        {
+            // Arrange
+            // This is a guid from one of the seeded seasons
+            var seasonId = new Guid("a78ca768-37e6-4ca7-889d-2c00158bb382");
+            Season? season = new Season { Id = seasonId, SeasonName = "Season to Delete", StartDate = DateTime.Today, PointStructureId = new Guid("d9db6444-f33f-4832-befe-46a17ea765cf") };
+            int result = 0;
+
+            using (var context = _fixture.CreateContext())
+            {
+                // Act and Assert
+                var unitOfWork = new UnitOfWork(context);
+
+                if (season != null)
+                {
+                    unitOfWork.Seasons.Add(season);
+                    unitOfWork.Save();
+
+                    unitOfWork.Seasons.Remove(season);
+                    result = unitOfWork.Save();
+                }
+
+                // If the result is greater than zero, we now the data has been affected which means it's been deleted
+                Assert.True(result > 0);
+            }
+        }
+
+        [Fact]
+        public void CannotRemoveSeasonIfTheSeasonHasGames()
+        {
+            // Arrange
+            // This is a guid from one of the seeded seasons
+            Season? season = new Season { SeasonName = "Season to Delete with Games", StartDate = DateTime.Today, PointStructureId = new Guid("d9db6444-f33f-4832-befe-46a17ea765cf") };
+            Game? game = new Game { Id = new Guid("6bd7d582-b11b-4ac3-9f07-e3b517ab561b"), Buyin = 35, Fee = 5, GameDateTime = DateTime.Now.AddDays(7), GameDetails = "Test Game", GameTitle = "Test Game", GameTypeId = new Guid("dbb07104-cffa-4539-91ce-1d0e5ecce2e0"), VenueId = new Guid("63c0255e-ecde-4edf-8a7f-3ecf026bba3d"), PublishResults = false };
+            season.Games.Add(game);
+
+            using (var context = _fixture.CreateContext())
+            {
+                // Act and Assert
+                var unitOfWork = new UnitOfWork(context);
+
+                if (season != null)
+                {
+                    unitOfWork.Seasons.Add(season);
+                    unitOfWork.Save();
+
+                    unitOfWork.Seasons.Remove(season);
+
+                    var exception = Assert.Throws<DbUpdateException>(() => unitOfWork.Save());
+                    var sqlException = exception.InnerException as SqlException;
+
+                    if (sqlException != null)
+                    {
+                        // If we get a sqlException of 2601 that means that the insert failed due to the constraint which is what this test is ensuring
+                        Assert.Equal(2601, sqlException.Number);
+                    }
+                }
+            }
+        }
     }
 }
