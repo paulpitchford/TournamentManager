af77e8625fd78cc19078f20b1bdcad5a16d9635e
Paul Pitchford <paul.pitchford@yourhomecare.co.uk>
2023-03-04 15:32:41 +0000

    Added delete tests to Venues

--- TournamentManager.Tests/RepositoryTests/VenueRepositoryTests.cs
+++ TournamentManager.Tests/RepositoryTests/VenueRepositoryTests.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Data.SqlClient;
+﻿using EntityFramework.Exceptions.Common;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using TournamentManager.DataAccess.UnitOfWork;
 using TournamentManager.Infrastructure.Entities;
@@ -75,6 +76,66 @@ namespace TournamentManager.Tests.RepositoryTests
 
                 // If the udpdate is successful the unit of work save method will return 1 to indicate 1 record was saved
                 Assert.Equal(1, unitOfWork.Save());
+            }
+        }
+
+        [Fact]
+        public void CanRemoveVenue()
+        {
+            // Arrange
+            // This is a guid from one of the seeded venues
+            Venue? venue = new Venue { VenueName = "Delete Test Venue", Address = "High Pavement", Town = "Sutton in Ashfield", County = "Nottinghamshire", PostCode = "NG17 4BA" };
+            int result = 0;
+
+            using (var context = _fixture.CreateContext())
+            {
+                // Act and Assert
+                var unitOfWork = new UnitOfWork(context);
+
+                if (venue != null)
+                {
+                    unitOfWork.Venues.Add(venue);
+                    unitOfWork.Save();
+
+                    unitOfWork.Venues.Remove(venue);
+                    result = unitOfWork.Save();
+                }
+
+                // If the result is greater than zero, we now the data has been affected which means it's been deleted
+                Assert.True(result > 0);
+            }
+        }
+
+        [Fact]
+        public void CannotRemoveVenueIfTheVenueHasGames()
+        {
+            // Arrange
+            Guid venueId = new Guid("6ad5b41c-c782-4aad-b55c-5249f63f07b3");
+            Venue? venue = new Venue { Id = venueId, VenueName = "Delete Test Venue", Address = "High Pavement", Town = "Sutton in Ashfield", County = "Nottinghamshire", PostCode = "NG17 4BA" };
+            Game? game = new Game { Id = Guid.NewGuid(), SeasonId = new Guid("1b0c1ad0-e4f5-4fb6-98a4-e5e2a2d5e24e"), Buyin = 35, Fee = 5, GameDateTime = DateTime.Now.AddDays(7), GameDetails = "Test Game", GameTitle = "Test Game", GameTypeId = new Guid("dbb07104-cffa-4539-91ce-1d0e5ecce2e0"), VenueId = new Guid("6ad5b41c-c782-4aad-b55c-5249f63f07b3"), PublishResults = false };
+            venue.Games.Add(game);
+
+            using (var context = _fixture.CreateContext())
+            {
+                // Act and Assert
+                var unitOfWork = new UnitOfWork(context);
+
+                if (venue != null)
+                {
+                    unitOfWork.Venues.Add(venue);
+                    unitOfWork.Save();
+
+                    unitOfWork.Venues.Remove(venue);
+
+                    var exception = Assert.Throws<CannotInsertNullException>(() => unitOfWork.Save());
+                    var sqlException = exception.InnerException as SqlException;
+
+                    if (sqlException != null)
+                    {
+                        // If we get a sqlException of 515 that means that the remove failed because of the games that exist on the entity
+                        Assert.Equal(515, sqlException.Number);
+                    }
+                }
             }
         }
     }
